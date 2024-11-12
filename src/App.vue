fd4be29c7d87a0b8209a7eeefa20f01e09f7a0f2
alberthgrande <alberthgrande98@gmail.com>
2024-11-12 18:41:31 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mx-auto">
+    <NavBar />
+    <div class="my-6">
+      <input
+        class="w-full max-w-md px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+        type="text"
+        v-model="searchCountry"
+        placeholder="Search for a country..."
+      />
+    </div>
+    <div
+      class="grid grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-content-center"
+    >
+      <div
+        v-for="country in filteredCountry"
+        :key="country.id"
+        class="rounded-lg shadow-md overflow-hidden"
+      >
+        <div>
+          <img
+            class="w-full h-48 object-cover"
+            :src="country.flags.svg"
+            :alt="country.name"
+          />
+        </div>
+        <div class="p-8">
+          <h3 class="text-lg font-semibold">{{ country.name }}</h3>
+          <p class="mt-2">
+            <strong>Population:</strong>
+            {{ country.population.toLocaleString() }}
+          </p>
+          <p><strong>Region:</strong> {{ country.region }}</p>
+          <p>
+            <strong>Capital:</strong>
+            {{ country.capital ? country.capital : "N/A" }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import userData from "./assets/data.json";
+import NavBar from "./components/NavBar.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    NavBar,
+  },
+  data() {
+    return {
+      countries: userData,
+      searchCountry: "",
+    };
+  },
+  computed: {
+    filteredCountry() {
+      return this.countries.filter((country) => {
+        return country.name
+          .toLowerCase()
+          .includes(this.searchCountry.toLowerCase());
+      });
+    },
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-8">
+    <nav class="flex justify-between">
+      <h1>Where in the world?</h1>
+      <button @click="toggleDark()">Dark Mode</button>
+    </nav>
+  </div>
+</template>
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+<style>
+.dark {
+  background: #242424;
+  color: #fff;
+}
+</style>
